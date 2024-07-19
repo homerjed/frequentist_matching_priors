edee014ff8bc460b21c0ccb4ba28f57be40e3a90
homerjed <jedhmr@gmail.com>
2024-07-19 18:15:20 +0200

    setup.py installs

--- setup.py
+++ setup.py
@@ -3,5 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name="frequentist_matching_priors",
     packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    package_dir={"" : "src"},
+    requires=[
+        "numpy",
+        "matplotlib",
+        "jax[cpu]",
+        "blackjax",
+        "tqdm",
+        "pandas",
+        "chainconsumer",
+        "tensorflow_probability"
+    ]
 )
